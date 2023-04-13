6a07a8b3c5fa5ff8f563595a7393e2eb5e9ed503
Merame <egoistgray@gmail.com>
2023-04-14 02:14:10 +0800

    fix(unit): autoplay delay to 5s

--- src/templates/unit.tsx
+++ src/templates/unit.tsx
@@ -76,7 +76,7 @@ export default function Unit(info: UnitInfo) {
             modules={[Navigation, Pagination, Autoplay, Zoom]}
             pagination={{ clickable: true }}
             zoom={{ toggle: true, maxRatio: 3 }}
-            autoplay={{ delay: 3000 }}
+            autoplay={{ delay: 5000 }}
             navigation
          >
             {info.images.map((image, index) => (
