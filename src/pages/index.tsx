1b3c637dc013c68f90aa9c4974f5f770ec348f28
Merame <egoistgray@gmail.com>
2023-04-14 00:07:08 +0800

    feat(metadata)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,27 @@ import Hero from "@/sections/HeroBanner";
 import Head from "next/head";
 import AboutUs from "@/sections/AboutUs";
 import Units from "@/sections/Units";
+import { NextSeo, WebPageJsonLd } from "next-seo";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
    return (
       <main>
-         <Head>
-            <title>Arayana Serpong</title>
-         </Head>
+         <NextSeo
+            title="Aryana Residence | Yenyen Studio"
+            description="Live vibrantly, live modernly. All you need is in here"
+            openGraph={{
+               type: "website",
+               title: "Aryana Residence",
+               description:
+                  "Live vibrantly, live modernly. All you need is in here"
+            }}
+         />
+         <WebPageJsonLd
+            id={"aryana"}
+            description={"Live vibrantly. Live modernly"}
+         />
          <Hero />
          <AboutUs />
          <Units />
