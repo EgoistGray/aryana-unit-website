2ea2b27f7295411590fdc271d749c4e93e4958ce
Merame <egoistgray@gmail.com>
2023-04-14 02:15:27 +0800

    fix(familia): duplicate image import

--- src/images/famillia.tsx
+++ src/images/famillia.tsx
@@ -3,8 +3,8 @@ import Balkoni from "@/assets/Cluster Megah/Famillia/Balkon.jpg";
 import Dapur from "@/assets/Cluster Megah/Famillia/Dapur.jpg";
 import KT1 from "@/assets/Cluster Megah/Famillia/KT 1 Lantai 1.jpg";
 import KT2 from "@/assets/Cluster Megah/Famillia/KT 2 Lantai 1.jpg";
-import KT3 from "@/assets/Cluster Megah/Famillia/KT 3 Lantai 2.jpg";
 import KT4 from "@/assets/Cluster Megah/Famillia/KT 3 Lantai 2.jpg";
+import KT3 from "@/assets/Cluster Megah/Famillia/KT 4 Lantai 2.jpg";
 import LivingRoom from "@/assets/Cluster Megah/Famillia/Living Room.jpg";
 import WorkRoom from "@/assets/Cluster Megah/Famillia/Ruang Kerja.jpg";
 import DiningRoom from "@/assets/Cluster Megah/Famillia/Ruang Makan.jpg";
@@ -22,7 +22,7 @@ const FamiliaImages = [
    KT1,
    KT2,
    KT3,
-   KT4
+   KT4,
 ];
 
 export default FamiliaImages;
